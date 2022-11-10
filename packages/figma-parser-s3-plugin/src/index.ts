2e813fa10404d2fac15b091d3aa7b68fcc4fbbbe
cerosnikita <113107108+cerosnikita@users.noreply.github.com>
2022-11-10 15:54:41 +0100

    fix cacher (#6)

--- packages/figma-parser-s3-plugin/src/index.ts
+++ packages/figma-parser-s3-plugin/src/index.ts
@@ -2,47 +2,47 @@ import { S3Cacher } from './s3-cacher'
 import { createLogger } from './services/logger'
 import S3 from './services/s3'
 
-import type { PathLocator } from './services/path-locator/path-locator.iface'
 import type { Logger, S3Service } from './types'
+import type { IPathLocator } from './types/path-locator'
 
-export type S3CacherOptions = {
+type S3CacherWithInstanceOptions = {
   logger?: Logger
-  s3service?: S3Service
-
-  accessKeyId?: string
-  secretAccessKey?: string
-  region?: string
+  s3service: S3Service
   verbose?: boolean
-
-  pathLocator: PathLocator
-
-  // outputDir: string
-  // cacheMapFile: string
-  // sourcesDir: string
-  // artifactsDir: string
-
+  pathLocator: IPathLocator
   uploadParallels: number
   downloadParallels: number
 }
+type S3CacherWithCredentialsOptions = {
+  logger?: Logger
+  accessKeyId: string
+  secretAccessKey: string
+  region: string
+  verbose?: boolean
+  pathLocator: IPathLocator
+  uploadParallels: number
+  downloadParallels: number
+  uploadBucket?: string
+  downloadBucket?: string
+  acl?: string
+}
+export type S3CacherOptions = S3CacherWithInstanceOptions | S3CacherWithCredentialsOptions
+
+export type { IPathLocator }
 
 export class S3Plugin {
   _logger: Logger
   _s3: S3Service
   _cacher: S3Cacher
-  _pathLocator: PathLocator
+  _pathLocator: IPathLocator
   _parallels: {
     upload: number
     download: number
   }
 
-  // _outputDir: string
-  // _cacheMapFile: string
-  // _sourcesDir: string
-  // _artifactsDir: string
-
   constructor(options: S3CacherOptions) {
     this._logger = options.logger ?? this._createLogger()
-    this._s3 = options.s3service ?? this._createS3(options)
+    this._s3 = 's3service' in options ? options.s3service : this._createS3(options)
     this._pathLocator = options.pathLocator
     this._parallels = {
       upload: options.uploadParallels ?? 5,
@@ -51,32 +51,11 @@ export class S3Plugin {
     this._cacher = new S3Cacher({
       s3Plugin: this,
     })
-
-    // this._outputDir = options.outputDir
-    // this._cacheMapFile = options.cacheMapFile
-    // this._sourcesDir = options.sourcesDir
-    // this._artifactsDir = options.artifactsDir
   }
 
   get cacher() {
     return this._cacher
   }
-
-  // get artifactsDir(): string {
-  //   return this._artifactsDir
-  // }
-
-  // get sourcesDir(): string {
-  //   return this._sourcesDir
-  // }
-
-  // get outputDir(): string {
-  //   return this._outputDir
-  // }
-
-  // get cacheMapFile(): string {
-  //   return this._cacheMapFile
-  // }
 
   get logger(): Logger {
     return this._logger
@@ -86,13 +65,16 @@ export class S3Plugin {
     return createLogger()
   }
 
-  private _createS3(options: S3CacherOptions) {
+  private _createS3(options: S3CacherWithCredentialsOptions) {
     return new S3({
       accessKeyId: options.accessKeyId,
       secretAccessKey: options.secretAccessKey,
       region: options.region,
       verbose: options.verbose,
       s3Plugin: this,
+      uploadBucket: options.uploadBucket,
+      downloadBucket: options.downloadBucket,
+      acl: options.acl,
     })
   }
 }
