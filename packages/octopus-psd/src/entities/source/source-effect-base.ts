df087fac7f1b1a43c103fae3c7ccb99853d88024
alexspevak <alexander.spevak@ceros.com>
2023-02-03 19:54:11 +0100

    move blend  modes to defaults

--- packages/octopus-psd/src/entities/source/source-effect-base.ts
+++ packages/octopus-psd/src/entities/source/source-effect-base.ts
@@ -1,6 +1,7 @@
 import { SourceEntity } from './source-entity.js'
 
 import type { RawEffectShadow } from '../../typings/raw'
+import type { DEFAULTS } from '../../utils/defaults.js'
 
 export class SourceEffectBase extends SourceEntity {
   protected _rawValue: RawEffectShadow | undefined
@@ -9,8 +10,8 @@ export class SourceEffectBase extends SourceEntity {
     super(rawValue)
   }
 
-  get blendMode(): string | undefined {
-    return this._rawValue?.Md
+  get blendMode(): keyof typeof DEFAULTS.BLEND_MODES | undefined {
+    return this._rawValue?.Md as keyof typeof DEFAULTS.BLEND_MODES | undefined
   }
 
   get enabled(): boolean {
