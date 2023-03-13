4cf0100ffaa5528433a5b74a0d087f98a5f64132
alexspevak <alexander.spevak@ceros.com>
2023-03-13 17:18:13 +0100

    v3.0.0-rc.5-tarball-02

--- packages/octopus-ai/examples/web/vite.web-test.js
+++ packages/octopus-ai/examples/web/vite.web-test.js
@@ -11,5 +11,5 @@ export default defineConfig({
   },
   resolve: {
     preserveSymlinks: true,
-  }
+  },
 })
--- packages/octopus-ai/prepare-release.mjs
+++ packages/octopus-ai/prepare-release.mjs
@@ -1,8 +1,6 @@
 import fs from 'fs/promises'
 import path from 'path'
 
-import tar from 'tar'
-
 import { default as pkg } from './package.json' assert { type: 'json' }
 
 const RELEASE_DIR = './release'
@@ -47,19 +45,6 @@ function preparePublicPackageJSON() {
   return fs.writeFile(path.join(RELEASE_DIR, 'package.json'), JSON.stringify({ ...pkg, ...RELEASE_PROPS }, null, 2))
 }
 
-async function packFiles() {
-  const content = await fs.readdir(RELEASE_DIR)
-  return tar.c(
-    {
-      gzip: true,
-      file: 'release.tgz',
-      cwd: RELEASE_DIR,
-      prefix: 'package',
-    },
-    content.map((file) => `./${file}`)
-  )
-}
-
 function copyDMTStoDTS() {
   return fs.copyFile(path.join(RELEASE_DIR, 'index.d.mts'), path.join(RELEASE_DIR, 'index.d.ts'))
 }
@@ -68,7 +53,6 @@ async function release() {
   await prepareReleaseDir()
   await preparePublicPackageJSON()
   await copyDMTStoDTS()
-  await packFiles()
 }
 
 release()
