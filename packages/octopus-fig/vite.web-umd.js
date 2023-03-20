5245ebf64c6fcf5d7be6d567aeda815e6c742517
Jan Návrat <jan.navrat@ceros.com>
2023-03-20 12:09:15 +0100

    octopus-figma web-umg.js build

--- packages/octopus-fig/vite.web-umd.js
+++ packages/octopus-fig/vite.web-umd.js
@@ -3,21 +3,29 @@ import path, { dirname } from 'path'
 import { fileURLToPath } from 'url'
 
 import { defineConfig } from 'vite'
+import dts from 'vite-plugin-dts'
 
 export default defineConfig({
   build: {
     lib: {
       entry: path.resolve(dirname(fileURLToPath(new URL(import.meta.url))), 'src/index-web.ts'),
       name: 'OctopusFig',
-      fileName: (format) => `octopus-fig-web.${format}.js`,
+      fileName: () => `web-umd.js`,
       formats: ['umd'],
     },
     commonjsOptions: { include: [/node_modules/, /@opendesign\/octopus-common/] },
-    outDir: 'examples/web',
+    outDir: 'release',
     emptyOutDir: false,
+    target: 'es6',
+    rollupOptions: {},
     // minify: 'none'
   },
   resolve: {
     preserveSymlinks: true,
   },
+  plugins: [
+    dts({
+      insertTypesEntry: true,
+    }),
+  ],
 })
